4fe9ccf003b5302d857ac72edd77fe48ee566406
Keriot <selchaneckristijan@gmail.com>
2025-02-12 15:46:42 +0100

    a

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 // db.js
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://mongo:27017/schoolDB";  // Change localhost to mongo, which is the service name in docker-compose
+const mongoURI = "mongodb://mongo-service.default.svc.cluster.local:27017/schoolDB";
+// Change localhost to mongo, which is the service name in docker-compose
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,7 +16,19 @@ const subjectSchema = new mongoose.Schema({
     description: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 });
+const connectWithRetry = () => {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Connected to MongoDB");
+    }).catch(err => {
+        console.error("Failed to connect to MongoDB, retrying...", err);
+        setTimeout(connectWithRetry, 5000); // Retry connection after 5 seconds
+    });
+};
 
+connectWithRetry();
 const Subject = mongoose.model("Subject", subjectSchema);
 
 module.exports = { Subject };
--- server.js
+++ server.js
@@ -78,4 +78,5 @@ app.get("*", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log('test')
 });
